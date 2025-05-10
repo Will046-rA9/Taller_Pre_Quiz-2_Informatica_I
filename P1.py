0b7bf10f605e8aacaf4890b5794e9b74a12b8fa9
Will046-rA9 <william.mherrera@udea.edu.co>
2025-05-10 08:47:32 -0500

    Punto1

--- P1.py
+++ P1.py
@@ -7,3 +7,5 @@ m1 = np.random.randint([100, 100, 10, 12])
 print(f"La matriz tiene {m1.size} elementos")
 print(f"La matriz tiene {m1.ndim} dimensiones")
 print(f"La matriz tiene la forma: {m1.shape}")
+
+print("\n\nPUNTO 2: Crea una copia de la matriz creada en el ítem anterior (usar método copy) de solo 3 dimensiones (“Cortando una de las dimensiones”)")
